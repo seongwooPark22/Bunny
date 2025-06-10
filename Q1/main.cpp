a7550e5dab09f7ae909a79e2d6dcb67c103ba7cb
seongwooPark22 <dnflwlq14@konkuk.ac.kr>
2025-06-11 04:19:06 +0900

    first commit

---
+++ Q1/main.cpp
@@ -0,0 +1,325 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/freeglut.h>
+
+#include <GL/glu.h>
+#include <GL/glext.h>
+
+#define GLFW_INCLUDE_GLU
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <queue>
+#include <float.h>
+
+int WIDTH = 1280;
+int HEIGHT = 1280;
+
+float  					gTotalTimeElapsed = 0;
+int 					gTotalFrames = 0;
+GLuint 					gTimer;
+
+struct Triangle
+{
+	unsigned int 	indices[3];
+};
+
+std::vector<Triangle>	gTriangles;
+std::vector<glm::vec3> vertex_position_buffer;
+std::vector<glm::vec3> vertex_normal_buffer;
+
+float PI = 3.141592f;
+
+using namespace glm;
+
+void tokenize(char* string, std::vector<std::string>& tokens, const char* delimiter)
+{
+	char* token = strtok(string, delimiter);
+	while (token != NULL)
+	{
+		tokens.push_back(std::string(token));
+		token = strtok(NULL, delimiter);
+	}
+}
+
+int face_index(const char* string)
+{
+	int length = strlen(string);
+	char* copy = new char[length + 1];
+	memset(copy, 0, length + 1);
+	strcpy(copy, string);
+
+	std::vector<std::string> tokens;
+	tokenize(copy, tokens, "/");
+	delete[] copy;
+	if (tokens.front().length() > 0 && tokens.back().length() > 0 && atoi(tokens.front().c_str()) == atoi(tokens.back().c_str()))
+	{
+		return atoi(tokens.front().c_str());
+	}
+	else
+	{
+		printf("ERROR: Bad face specifier!\n");
+		exit(0);
+	}
+}
+
+void load_mesh(std::string fileName)
+{
+	std::ifstream fin(fileName.c_str());
+	if (!fin.is_open())
+	{
+		printf("ERROR: Unable to load mesh from %s!\n", fileName.c_str());
+		exit(0);
+	}
+
+	float xmin = FLT_MAX;
+	float xmax = -FLT_MAX;
+	float ymin = FLT_MAX;
+	float ymax = -FLT_MAX;
+	float zmin = FLT_MAX;
+	float zmax = -FLT_MAX;
+
+	while (true)
+	{
+		char line[1024] = { 0 };
+		fin.getline(line, 1024);
+
+		if (fin.eof())
+			break;
+
+		if (strlen(line) <= 1)
+			continue;
+
+		std::vector<std::string> tokens;
+		tokenize(line, tokens, " ");
+
+		if (tokens[0] == "v")
+		{
+			float x = atof(tokens[1].c_str());
+			float y = atof(tokens[2].c_str());
+			float z = atof(tokens[3].c_str());
+
+			xmin = std::min(x, xmin);
+			xmax = std::max(x, xmax);
+			ymin = std::min(y, ymin);
+			ymax = std::max(y, ymax);
+			zmin = std::min(z, zmin);
+			zmax = std::max(z, zmax);
+
+			vertex_position_buffer.push_back(glm::vec3(x, y, z));
+		}
+		else if (tokens[0] == "vn")
+		{
+			float x = atof(tokens[1].c_str());
+			float y = atof(tokens[2].c_str());
+			float z = atof(tokens[3].c_str());
+
+			vertex_normal_buffer.push_back(glm::vec3(x, y, z));
+		}
+		else if (tokens[0] == "f")
+		{
+			unsigned int a = face_index(tokens[1].c_str());
+			unsigned int b = face_index(tokens[2].c_str());
+			unsigned int c = face_index(tokens[3].c_str());
+
+			Triangle triangle;
+			triangle.indices[0] = a - 1;
+			triangle.indices[1] = b - 1;
+			triangle.indices[2] = c - 1;
+			gTriangles.push_back(triangle);
+		}
+	}
+
+	fin.close();
+
+	printf("Loaded mesh from %s. (%lu vertices, %lu normals, %lu triangles)\n", fileName.c_str(), vertex_position_buffer.size(), vertex_normal_buffer.size(), gTriangles.size());
+	printf("Mesh bounding box is: (%0.4f, %0.4f, %0.4f) to (%0.4f, %0.4f, %0.4f)\n", xmin, ymin, zmin, xmax, ymax, zmax);
+}
+
+void init_timer()
+{
+	glGenQueries(1, &gTimer);
+}
+
+void start_timing()
+{
+	glBeginQuery(GL_TIME_ELAPSED, gTimer);
+}
+
+float stop_timing()
+{
+	glEndQuery(GL_TIME_ELAPSED);
+
+	GLint available = GL_FALSE;
+	while (available == GL_FALSE)
+		glGetQueryObjectiv(gTimer, GL_QUERY_RESULT_AVAILABLE, &available);
+
+	GLint result;
+	glGetQueryObjectiv(gTimer, GL_QUERY_RESULT, &result);
+
+	float timeElapsed = result / (1000.0f * 1000.0f * 1000.0f);
+	return timeElapsed;
+}
+
+void resize_callback(GLFWwindow*, int nw, int nh) 
+{
+	WIDTH = nw;
+	HEIGHT = nh;
+
+	glViewport(0, 0, nw, nh);
+}
+
+void set_material(glm::vec4 ka, glm::vec4 kd, glm::vec4 ks, float specular_power) {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, &ka[0]);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, &kd[0]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, &ks[0]);
+	glMaterialf(GL_FRONT, GL_SHININESS, specular_power);
+}
+
+
+int main(int argc, char* argv[])
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Q1-OpenGL Bunny", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+
+	glfwSetFramebufferSizeCallback(window, resize_callback);
+	resize_callback(NULL, WIDTH, HEIGHT);
+
+	glewExperimental = true;
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		printf("SOMETHING IS WRONG\n");
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_NORMALIZE);
+	glDepthFunc(GL_LESS);
+
+	init_timer();
+
+	load_mesh("bunny.obj");
+
+	std::vector<int> indices;
+	for (int triangle_ind = 0; triangle_ind < gTriangles.size(); triangle_ind++) {
+		indices.push_back(gTriangles[triangle_ind].indices[0]);
+		indices.push_back(gTriangles[triangle_ind].indices[1]);
+		indices.push_back(gTriangles[triangle_ind].indices[2]);
+	}
+
+	float light_pos[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+	float Ia[] = { 0.2f, 0.2f, 0.2f, 0.0f };
+	float la[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	float ld[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+	float ls[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+
+	set_material(glm::vec4(1.0f, 1.0f, 1.0f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 0.0f), glm::vec4(0.0f, 0.0f, 0.0f, 0.0f), 0.0f);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Ia);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, la);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, ld);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, ls);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//Camera
+	gluLookAt(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f);
+
+	//Model
+	float scale = 10.0f;
+	glTranslatef(0.1f, -1.0f, -1.5f);
+	glScalef(scale, scale, scale);
+
+	//Projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	float l, r, b, t, n, f;
+	l = -0.1f; r = 0.1f; b = -0.1f, t = 0.1f, n = 0.1f, f = 1000.0f;
+	glFrustum(l, r, b, t, n, f);
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	float avg_fps = 0.0f;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		start_timing();
+
+		glBegin(GL_TRIANGLES);
+		for (int i = 0; i < indices.size(); i++) {
+			glm::vec3 v = vertex_position_buffer[indices[i]];
+			glm::vec3 v_n = vertex_normal_buffer[indices[i]];
+
+			v_n = normalize(v_n);
+
+			glNormal3f(v_n.x, v_n.y, v_n.z);
+			glVertex3f(v.x, v.y, v.z);
+		}
+		glEnd();
+
+		float timeElapsed = stop_timing();
+		gTotalFrames++;
+		gTotalTimeElapsed += timeElapsed;
+		float fps = gTotalFrames / gTotalTimeElapsed;
+		char string[1024] = { 0 };
+		sprintf(string, "Q1-OpenGL Bunny: %0.2f FPS", fps);
+		avg_fps += fps;
+		glfwSetWindowTitle(window, string);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS
+			|| glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	}
+
+	avg_fps = avg_fps / (float)gTotalFrames;
+	printf("\nTotal Frame Passed : %d\nQ1 Average FPS : %f\n", gTotalFrames, avg_fps);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,50 @@
+# Assignment8 - Bunny
+
+*I used provided Sample Code from eCampus to display image. and I also used provided code 'frame_timer.cpp' and 'load_mesh.cpp' from eCampus.*
+
+The Result Screenshots are in the 'Screenshots' directory in the repository as you can see.
+
+## Instructions
+
+1. Clone this repository, and go to the directory of this repository cloned into
+2. Open OpenglViewer.sln, There are three Projects in this Solution, Q1 and Q2
+3. Right Click the Project you want to build and run from Solution Explorer and Click 'Set as Startup Project' from appeared drop-down menu
+4. Build Solution. Click 'Build > Build Solution' on the menu bar
+5. Run the Program. Click 'Debug > Start debugging' on the menu bar or press F5 to run the program
+6. The program will be running and will display the result
+
+## Result Screenshots
+
+### Q1 - Results
+![Q1ResultScreenshot](./Screenshots/ScreenshotQ1-main.png)
+#### Average FPS
+- Attempt1
+![Q1ResultScreenshot](./Screenshots/ScreenshotQ1-avgFPS1.png)
+- Attempt2
+![Q1ResultScreenshot](./Screenshots/ScreenshotQ1-avgFPS2.png)
+- Attempt3
+![Q1ResultScreenshot](./Screenshots/ScreenshotQ1-avgFPS3.png)
+
+Average of 3 attempts : 563.340271 Average FPS
+
+### Q2 - Results
+![Q2ResultScreenshot](./Screenshots/ScreenshotQ2-main.png)
+#### Average FPS
+- Attempt1
+![Q2ResultScreenshot](./Screenshots/ScreenshotQ2-avgFPS1.png)
+- Attempt2
+![Q2ResultScreenshot](./Screenshots/ScreenshotQ2-avgFPS2.png)
+- Attempt3
+![Q2ResultScreenshot](./Screenshots/ScreenshotQ2-avgFPS3.png)
+
+Average of 3 attempts : 818.156534667 Average FPS
+
+### Summary
+Q2's way is faster and more efficient than Q1's way.
+Q2 is better. (about 1.45 times)
+
+Q1 is using glVertex3f, and glNormal3f to transfer datas to GPU every frame,
+but Q2 is using VAO and VBOs to transfer datas to GPU before rendering process and reuse them.
+
+#### Please Check more detailed report on ecampus.
+I uploaded more detailed report on ecampus. please check that report.
